485d909a6f9159099b41867530cab0f3dcc2d168
zaknisdev <zakianisf1710@gmail.com>
2026-04-15 00:01:13 +0700

    Add project files.

---
+++ koneksiDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace SleepWise
+{
+    public class koneksiDB
+    {
+        static string connectionString = string.Format("Server=localhost;database=SleepWiseDB;UID=root;" + "Password=andasiapa");
+        
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database gagal terknoneksi lee: " + ex.Message);
+                return null;
+            }
+            
+        }
+
+    }
+}
