cec0452cb7c048828a6392e2bcd0d81ffa8901c9
zaknisdev <zakianisf1710@gmail.com>
2026-04-15 02:43:20 +0700

    Design untuk tracking tidur user

---
+++ FormSleepTracker.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SleepWise
+{
+    partial class FormSleepTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpTidur = new System.Windows.Forms.DateTimePicker();
+            this.dtpBangun = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpTidur
+            // 
+            this.dtpTidur.CustomFormat = "HH : mm";
+            this.dtpTidur.Location = new System.Drawing.Point(134, 100);
+            this.dtpTidur.Name = "dtpTidur";
+            this.dtpTidur.Size = new System.Drawing.Size(200, 26);
+            this.dtpTidur.TabIndex = 1;
+            this.dtpTidur.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dtpBangun
+            // 
+            this.dtpBangun.CustomFormat = "HH : mm";
+            this.dtpBangun.Location = new System.Drawing.Point(134, 147);
+            this.dtpBangun.Name = "dtpBangun";
+            this.dtpBangun.Size = new System.Drawing.Size(200, 26);
+            this.dtpBangun.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Waktu Tidur:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Waktu Bangun:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(477, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 75);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Simpan";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(134, 47);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tanggal:";
+            // 
+            // FormSleepTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpBangun);
+            this.Controls.Add(this.dtpTidur);
+            this.Name = "FormSleepTracker";
+            this.Text = "FormSleepTracker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DateTimePicker dtpTidur;
+        private System.Windows.Forms.DateTimePicker dtpBangun;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+    }
+}
