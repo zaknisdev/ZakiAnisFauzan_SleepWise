e5d88dbd2720141e6d103b72c534230c5e1c88a7
zaknisdev <zakianisf1710@gmail.com>
2026-04-15 01:26:53 +0700

    bikin method di tombol buat akunnya

--- FormSignup.cs
+++ FormSignup.cs
@@ -7,12 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace SleepWise
 {
-    public partial class FormSignup : Form
+    
+    public partial class FormSignUp : Form
     {
-        public FormSignup()
+        koneksiDB db = new koneksiDB();
+        public FormSignUp()
         {
             InitializeComponent();
         }
@@ -26,5 +29,57 @@ namespace SleepWise
         {
 
         }
+
+        private void btnSignup_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" || txtPassword.Text == "" || txtNamaLengkap.Text == "")
+            {
+                MessageBox.Show("Diisi semua ya ganteng!");
+                return; 
+            }
+
+            
+            using (MySqlConnection conn = db.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+
+                    
+                    string query = "INSERT INTO ms_user (username, password, nama_lengkap, role, target_tidur_jam) " +
+                                   "VALUES (@user, @pass, @nama, 'Pengguna', 8)";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@user", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@pass", txtPassword.Text);
+                    cmd.Parameters.AddWithValue("@nama", txtNamaLengkap.Text);
+
+                    
+                    int hasil = cmd.ExecuteNonQuery();
+
+                    if (hasil > 0)
+                    {
+                        MessageBox.Show("Akun berhasil dibuat! Silakan login dengan akun baru Anda.", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        FormLogin formLogin = new FormLogin();
+                        formLogin.Show();
+                        this.Hide();
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    
+                    if (ex.Number == 1062)
+                    {
+                        MessageBox.Show("Username sudah dipakai orang lain! Jangan nyontek orang lain beb!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Gagal nyambung ke database: " + ex.Message);
+                    }
+                }
+            }
+        }
     }
+    
 }
